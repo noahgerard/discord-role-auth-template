c6914d7035fbf4ecdc568f0d95ea3f94f5a91a3e
HelixDevelopment <helix.dev.xyz@gmail.com>
2024-03-19 09:34:24 -0400

    remove unneccesary router code

--- src/app/components/LogoutButton.tsx
+++ src/app/components/LogoutButton.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import { useRouter } from 'next/navigation';
-
 const LogoutButton = () => {
-  const router = useRouter();
-
   const logout = async () => {
-    console.log("logout");
     const response = await fetch("/api/session/", {
       method: "DELETE",
     });
